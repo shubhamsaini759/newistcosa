f22c5e91547b241d6e41ab951099e777518f7d87
shubham saini <shubhamsaini759@gmail.com>
2023-02-15 23:24:14 +0530

    baseurl

--- src/Components/GlobalComp/test.js
+++ src/Components/GlobalComp/test.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import ImgCrop from 'antd-img-crop';
-import { Upload } from 'antd';
-
-
+import React, { useEffect, useState } from "react";
+import ImgCrop from "antd-img-crop";
+import { Upload } from "antd";
 
 const getBase64 = (file) =>
   new Promise((resolve, reject) => {
@@ -10,71 +8,44 @@ const getBase64 = (file) =>
     reader.readAsDataURL(file);
     reader.onload = () => {
       resolve(reader.result);
-      console.log(reader.result)
+      // console.log(reader.result);
     };
     reader.onerror = (error) => reject(error);
   });
 
-// const getSrcFromFile = (file) => {
-//   return new Promise((resolve) => {
-//     const reader = new FileReader();
-//     reader.readAsDataURL(file.originFileObj);
-//     reader.onload = () => resolve(reader.result);
-//   });
-// };
-
-
 const Test = () => {
   const [fileList, setFileList] = useState([]);
+  const [detail, setDetail] = useState("");
 
-  const onChange =({ fileList: newFileList }) => {
+  const onChange = ({ fileList: newFileList }) => {
     setFileList(newFileList);
+    convertBase64(newFileList[0]);
   };
 
- 
-  
-
-  // const onPreview = async (file) => {
-  //   const src = file.url || (await getSrcFromFile(file));
-  //   const imgWindow = window.open(src);
-
-  //   if (imgWindow) {
-  //     const image = new Image();
-  //     image.src = src;
-  //     imgWindow.document.write(image.outerHTML);
-  //   } else {
-  //     window.location.href = src;
-  //   }
-  // };
-
-  const [previewImage, setPreviewImage] = useState("");
-  
- 
-
-  const onPreview = async (file) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
-      console.log(file.preview,'url')
-    }
-
-    // setPreviewImage(file.url || (file.preview));
-
+  const convertBase64 = async (file) => {
+    file.preview = await getBase64(file.originFileObj);
+    setDetail(file.preview);
   };
+  const onPreview = () => {
+    window.open(detail);
+  };
+  // console.log(detail, "detail");
 
   return (
-    <ImgCrop grid rotate  >
-      <Upload
-        action=""
-        listType="picture-card"
-        fileList={fileList}
-        onChange={onChange}
-        onPreview={onPreview}
-      
-      >
-        {fileList.length < 1 && '+ Upload'}
-      </Upload>
-    </ImgCrop>
+    <>
+      <ImgCrop grid rotate>
+        <Upload
+          action=""
+          listType="picture-card"
+          fileList={fileList}
+          onChange={onChange}
+          onPreview={onPreview}
+        >
+          {fileList.length < 1 && "+ Upload"}
+        </Upload>
+      </ImgCrop>
+    </>
   );
 };
 
-export default Test
+export default Test;
